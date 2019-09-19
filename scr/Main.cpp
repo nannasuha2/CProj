d2b1495b24cd7b3d3ed788d4bea3e6f46474742f
Nasuha <nannasuha2@gmail.com>
2019-09-19 12:20:51 +0800

    update

--- scr/Main.cpp
+++ scr/Main.cpp
@@ -1,11 +1,21 @@
 #include<iostream>
-#include "Funcs.h"
+#include<cstring>
+//#include "Funcs.h"
+#include "Funcs2.h"
+
 
 
 int main()
 {
-	unsigned long long int sum = pEuler_SumSq(100);
-	std::cout << sum << std::endl;
+	//unsigned long long int sum = pEuler_SumSq(100);
+	//std::cout << sum << std::endl;
 	//std::cin.get();
+	//cin.get();
+	vector<int> a1 = { 1,2 };
+	vector<int> a2 = { 3,4 };
+	cout << LC_MedianTwoSortedArray(a1, a2);
+	//cout << LC_longestSubStr("pwwkew");
+
+
 	return 0;
 }
